39c1f584bb5c62d7e5791e81dc979eb632674e2d
Steven Lee <stevecreated@gmail.com>
2019-06-20 12:56:16 -0700

    added prisma and graphql yoga bindings

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+//TODO use express middleware to handle cookies (JWT)
+//TODO use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  info => {
+    console.log(`server is now running on port http:/localhost:${info.port}`);
+  }
+);
