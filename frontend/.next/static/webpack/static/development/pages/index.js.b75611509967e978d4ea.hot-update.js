2b3c81ee7a7ef3a104c554b57d7917b23b895e5e
Steven Lee <stevecreated@gmail.com>
2019-06-20 10:59:20 -0700

    initial backend and frontend

---
+++ frontend/.next/static/webpack/static/development/pages/index.js.b75611509967e978d4ea.hot-update.js
@@ -0,0 +1,33 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _styles_base_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../styles/base.scss */ "./styles/base.scss");
+/* harmony import */ var _styles_base_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_styles_base_scss__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/Users/stevenlee/source/cache-store/frontend/pages/index.js";
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "example",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "Hello World!");
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.b75611509967e978d4ea.hot-update.js.map
---
+++ frontend/pages/index.js
@@ -0,0 +1,3 @@
+import "../styles/base.scss";
+
+export default () => <div className="example">Hello World!</div>;
